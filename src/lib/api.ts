417309c98cad3b38b5f188792546baf760d98a7e
Suthan4 <harisuthan268@gmail.com>
2025-11-16 08:17:26 +0530

    api updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5005/api";
+  // process.env.NEXT_PUBLIC_API_URL || "http://localhost:5005/api";
+  process.env.NEXT_PUBLIC_API_URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
