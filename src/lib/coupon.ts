a59ed083d7da1535c8094ea02462cfe1f6630949
Suthan4 <harisuthan268@gmail.com>
2025-11-20 12:50:52 +0530

    updated url

--- src/lib/coupon.ts
+++ src/lib/coupon.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  // process.env.NEXT_PUBLIC_API_URL
-  "http://localhost:5005/api";
+  process.env.NEXT_PUBLIC_API_URL
+  // "http://localhost:5005/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
